89aab404000106984448e96b7cc02900bd94ff83
zctao <zt75@cornell.edu>
2017-11-08 10:53:46 -0600

    Some scripts for training

---
+++ ttHTauTauMVA/test/train_sklearn_bdt_1l2tau.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sklearn_train.py ttV 1l2tau -c -t -e -i ~/nobackup/mvaNtuples/ -o ~/public_html/BDTNov17/sklearn/1l2tau/ttV/ -w u -n
+sklearn_train.py ttbar 1l2tau -c -t -e -i ~/nobackup/mvaNtuples/ -o ~/public_html/BDTNov17/sklearn/1l2tau/ttbar/ -w u -n
---
+++ ttHTauTauMVA/test/train_sklearn_bdt_2lss1tau.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sklearn_train.py ttV 2lss1tau -c -t -e -i ~/nobackup/mvaNtuples/ -o ~/public_html/BDTNov17/sklearn/2lss1tau/ttV/ -w u -n
+sklearn_train.py ttbar 2lss1tau -c -t -e -i ~/nobackup/mvaNtuples/ -o ~/public_html/BDTNov17/sklearn/2lss1tau/ttbar/ -w u -n
